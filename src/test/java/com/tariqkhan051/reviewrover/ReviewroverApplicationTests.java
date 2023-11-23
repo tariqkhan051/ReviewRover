c7943236c707c5105ca65664c79f46fe52c726f5
Tariq Khan <tariqkhan051@outlook.com>
2023-11-24 03:19:55 +0500

    added hello world rest api

---
+++ src/test/java/com/tariqkhan051/reviewrover/ReviewroverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tariqkhan051.reviewrover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewroverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
